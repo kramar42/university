66721967901223a6fd6c9ad5581958e41f9e59d6
Alexey Kramarenko <kramar42@gmail.com>
2013-11-11 21:59:22 +0200

    Updated skeleton with code from stackoverflow answer

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lab-2 "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "EPAM lab 2. Produces/consumer."
+  :url "https://github.com/kramar42/lab-2"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
--- src/lab_2/core.clj
+++ src/lab_2/core.clj
@@ -1,7 +1,50 @@
 (ns lab-2.core
   (:gen-class))
 
+; Using this thread as example
+; http://stackoverflow.com/questions/2760017/producer-consumer-with-qualifications
+
+(def go-on? (atom true))
+(def queue (java.util.concurrent.LinkedBlockingQueue.))
+(def output (ref ()))
+
+; swap!
+; Atomically swaps the value of atom to be
+(defn- halt
+  ([] (halt 2000))
+  ([timeout]
+      (Thread/sleep timeout)
+      (swap! go-on? not)))
+
+; future
+; Takes a body of expressions and yields a future object that will
+; invoke the body in another thread, and will cache the result and
+; return it on all subsequent calls to deref/@.
+
+(defn- producer [vote]
+  (future
+    (while @go-on?
+      (.put queue vote)
+      (Thread/sleep (rand-int 200)))))
+
+(defn- consumer []
+  (future
+    (while @go-on?
+      (when-let [item (.poll queue)]
+        (Thread/sleep (rand-int 200))
+        (dosync (alter output conj item))))))
+
+; reset!
+; Sets the value of atom to newval without regard for the
+; current value. Returns newval.
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (reset! go-on? true)
+  (dorun (map #(%1 %2)
+              (repeat 5 producer)
+              (iterate inc 0)))
+  (dorun (repeatedly 2 consumer))
+  (halt)
+  output)
+
