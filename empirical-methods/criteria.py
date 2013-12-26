209c45593b61119e32b3eb84d521723bfc4c1918
Alexey Kramarenko <kramar42@gmail.com>
2013-12-26 02:14:23 +0200

    Removed ugly polinomial criteria code

--- empirical-methods/criteria.py
+++ empirical-methods/criteria.py
@@ -1,10 +1,12 @@
 #! /usr/bin/env python
 
+
 from random import randint
 from math import sqrt, pi, e
 from decimal import Decimal
 from sequence import to_digits
 from itertools import izip
+
 
 tau = Decimal(2*pi)
 e = Decimal(e)
@@ -34,64 +36,6 @@ def polynom_criteria(random, m=0):
         for j in xrange(1, k):
             X[i] += A[j] * X[i-j] % p
     return p ** k - 1
-
-
-def monotonicity_criteria(random, m=0):
-    def doMPos(A):
-        s_length = len(A)
-        i = 1
-        k = 0
-        i0 = -1
-        while i != n - 1:
-            if A[i - 1] > A[i]:
-                if i == n - 2:
-                    s_lenghts.append(k)
-                    k = 1;
-                    s_lenghts.append(k)
-                    break
-                k = i - i0 - 1
-                i0 = i
-                i = i + 2
-                s_lenghts.append(k)
-
-                if i == n - 1:
-                    if A[i - 1] > A[i]:
-                        k = 1
-                    else:
-                        k = 2
-                    s_lenghts.append(k)
-                    break
-
-                if i == n:
-                    break
-            else:
-                i += 1
-        return s_lenghts
-
-    n = 880
-    m = 8
-    s_lenghts = []
-    A = [k for i,k in izip(xrange(n), random)]
-    L = to_digits(doMPos(A))
-    s_number = [0] * n
-    PirsonS = 0
-    t = 5
-    pp = [0] * n
-    pr = [0] * n
-    pt = [0] * n
-
-    for i in L:
-        if i == L[i]:
-            s_number[i] += 1
-
-    for i in range(len(L)):
-        pp[i] = s_number[i] / len(L)
-        pr[i] = 1 / fact(L[i]) - 1 / fact(L[i] + 1)
-        if L[i] >= t:
-            pr[i] = 1 / fact(s_number[i])
-        PirsonS = +((pp[i] - pr[i]) ** 2) / pr[i]
-
-    return PirsonS * n
 
 
 def permutations_criteria(sequence, t):
